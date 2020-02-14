30587c4dfdec047a8a1a7f8abd8b6bdb3b3ce6bc
Tom Ellis <tom-git@jaguarpaw.co.uk>
2020-02-14 22:45:40 +0000

    Use dot

--- morans.hs
+++ morans.hs
@@ -79,7 +79,7 @@ deltas xv yv layers =
   f _          []         dvs          = dvs
   f (wm : wms) (zv : zvs) dvs@(dv : _) = f wms zvs $ (: dvs) $ zipWith
     (*)
-    [ sum $ zipWith (*) row dv | row <- wm ]
+    [ dot row dv | row <- wm ]
     (relu' <$> zv)
 
 eta :: Float
